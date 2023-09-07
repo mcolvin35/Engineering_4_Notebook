1c12bd321595aed5c75f69f0788501e6d9e0f28f
mcolvin35 <mcolvin35@charlottesvilleschools.org>
2023-09-07 10:28:52 -0400

    commented code

--- raspberry-pi/LAUNCHPAD/PART1.py
+++ raspberry-pi/LAUNCHPAD/PART1.py
@@ -1,6 +1,6 @@
 import time 
 
-for x in range(10, 0, -1):
-    print(x)
-    time.sleep(1)
-print("Liftoff!")
+for x in range(10, 0, -1): #loop 10 times, counting x down each time 
+    print(x) #print x 
+    time.sleep(1) #wait 1 second
+print("Liftoff!") #after 10 loops, print "Liftoff!"
