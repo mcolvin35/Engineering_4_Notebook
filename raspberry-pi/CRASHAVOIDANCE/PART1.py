31d4d51783c131eb3ad37b1d7fd90451df275c2d
mcolvin35 <mcolvin35@charlottesvilleschools.org>
2023-09-18 10:45:03 -0400

    started p1

--- raspberry-pi/CRASHAVOIDANCE/PART1.py
+++ raspberry-pi/CRASHAVOIDANCE/PART1.py
@@ -3,10 +3,11 @@
 import busio
 import adafruit_mpu6050
 import board
+import time
 
-sda_pin = board.31
-scl_pin = board.32
-i2c = busio.I2C(32, 31)
+sda_pin = (board.GP26)
+scl_pin = (board.GP27)
+i2c = busio.I2C(scl_pin, sda_pin)
 
 mpu = adafruit_mpu6050.MPU6050(i2c)
 accel = mpu.acceleration
