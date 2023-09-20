c3a24a95a61b5e3931e965b4e6c82a9a30123646
mcolvin35 <mcolvin35@charlottesvilleschools.org>
2023-09-20 09:30:22 -0400

    finished+commented

--- raspberry-pi/CRASHAVOIDANCE/PART1.py
+++ raspberry-pi/CRASHAVOIDANCE/PART1.py
@@ -5,15 +5,14 @@ import adafruit_mpu6050
 import board
 import time
 
-sda_pin = (board.GP26)
+sda_pin = (board.GP26) #set SDA as GP26 and SCL as GP27
 scl_pin = (board.GP27)
-i2c = busio.I2C(scl_pin, sda_pin)
+i2c = busio.I2C(scl_pin, sda_pin) #setup I2C connection
 
-mpu = adafruit_mpu6050.MPU6050(i2c)
-accel = mpu.acceleration
-gyro = mpu.gyro
-
+mpu = adafruit_mpu6050.MPU6050(i2c) #setup accelerometer 
 
 while True:
-    print(accel)
+    print(f"X:{mpu.acceleration[0]} Y:{mpu.acceleration[1]} Z:{mpu.acceleration[2]}") #print values
+    time.sleep(0.2) #wait so the values are readable
+
 
