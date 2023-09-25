868022622405abfcfaffa4f200055a8dd1787e8a
mcolvin35 <mcolvin35@charlottesvilleschools.org>
2023-09-25 10:42:57 -0400

    finished p3

--- raspberry-pi/CRASHAVOIDANCE/PART3.py
+++ raspberry-pi/CRASHAVOIDANCE/PART3.py
@@ -13,31 +13,42 @@ import terminalio
 import displayio
 
 displayio.release_displays()
-display_bus = displayio.I2CDisplay(i2c, device_address=0x3d, reset=board.GP17)
-display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
-
 
 sda_pin = (board.GP26) #set SDA as GP26 and SCL as GP27
 scl_pin = (board.GP27)
+
 i2c = busio.I2C(scl_pin, sda_pin) #setup I2C connection
 mpu = adafruit_mpu6050.MPU6050(i2c, address=0x68) #setup accelerometer 
+
+display_bus = displayio.I2CDisplay(i2c, device_address=0x3d, reset=board.GP17)
+display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
 
 led=digitalio.DigitalInOut(board.GP16) #assign LED to GP16 and set it as output
 led.direction=digitalio.Direction.OUTPUT 
 
 
-splash = displayio.Group()
 
-title = "ANGULAR VELOCITY"
-
-text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=5)
-splash.append(text_area)    
-
-display.show(splash)
 
 while True:
-    print(f"X:{mpu.acceleration[0]} Y:{mpu.acceleration[1]} Z:{mpu.acceleration[2]}") #print values
-    text_area.text = f"{mpu.acceleration}"
+    splash = displayio.Group()
+
+    title = "ANGULAR VELOCITY"
+
+    header = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=18, y=10)
+    X_display = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=45, y=25)
+    Y_display = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=45, y=40)
+    Z_display = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=45, y=55)
+    splash.append(header)
+    splash.append(X_display)    
+    splash.append(Y_display) 
+    splash.append(Z_display) 
+
+    header = "ANGULAR VELOCITY:"
+    X_display.text = f"X:{round(mpu.gyro[0], 3)}"
+    Y_display.text = f"Y:{round(mpu.gyro[1], 3)}"
+    Z_display.text = f"Z:{round(mpu.gyro[2], 3)}"
+
+    display.show(splash)
     time.sleep(0.2) #wait so the values are readable
     if mpu.acceleration[2] < 1 and mpu.acceleration[2] > -12: #if z acceleration is less than 1 (meaning board is on its side) and greater than -12 (so LED won't turn on when board is accelerating in Z)
         if mpu.acceleration[0] > +-5 or mpu.acceleration[1] > +-5: #also, if X and Y acceleration are greater than positive or negative 5 (meaning gravity is affecting one of them)
