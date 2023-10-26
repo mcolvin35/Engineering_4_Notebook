960a4777ff447fe9f5e5ed1675ffd00ea102f571
mcolvin35 <mcolvin35@charlottesvilleschools.org>
2023-10-26 10:46:21 -0400

    fixed code

--- raspberry-pi/LANDINGAREA/PART1.py
+++ raspberry-pi/LANDINGAREA/PART1.py
@@ -1,18 +1,17 @@
-
 def find_area(x1y1, x2y2, x3y3):
     try: 
-        x1y1 = txt.split(", ")
-        x2y2 = txt.split(", ")
-        x3y3 = txt.split(", ")
+        x1y1 = x1y1.split(",")
+        x2y2 = x2y2.split(",")
+        x3y3 = x3y3.split(",")
+        
+        x1 = float(x1y1[0])
+        y1 = float(x1y1[1])
+        x2 = float(x2y2[0])
+        y2 = float(x2y2[1])
+        x3 = float(x3y3[0])
+        y3 = float(x3y3[1])
 
-        x1 = float(x1)
-        y1 = float(y1)
-        x2 = float(x2)
-        y2 = float(y2)
-        x3 = float(x3)
-        y3 = float(y3)
-
-        area = (1/2)|x1(y2-y3)+x2(y3-y1)+x3(y1-y2)|
+        area = (1/2)*abs(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))
         
         return area
     except: 
@@ -26,10 +25,10 @@ while True:
     val2 = input("Enter second coordinate:")
     val3 = input("Enter third coordinate:")
 
-    area = triangle_area(val1, val2, val3)
+    area = find_area(val1, val2, val3)
 
-    if area == 0;
-        Continue
+    if area == 0:
+        continue
     
     else: 
         print(f"The area of the triangle with vertices {val1}, {val2}, {val3} is {area} square km.")
