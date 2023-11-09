5ef0749a9c5521abe72668e2ac5121c724733c2f
mcolvin35 <mcolvin35@charlottesvilleschools.org>
2023-11-09 10:01:44 -0500

    commented

--- raspberry-pi/MORSECODE/PART2.py
+++ raspberry-pi/MORSECODE/PART2.py
@@ -22,11 +22,11 @@ MORSE_CODE = { 'A':'.-', 'B':'-...',
     '!': '-.-.--'}  #dictionary for morse code translations
 
 modifier = 0.25
-dot_time = 1*modifier
-dash_time = 3*modifier
-between_taps = 1*modifier
-between_letters = 3*modifier
-between_words = 7*modifier
+dot_time = 1*modifier #dot time is 0.25 seconds
+dash_time = 3*modifier #dash time is 0.75 
+between_taps = 1*modifier #time between characters in one letter
+between_letters = 3*modifier #time between letters
+between_words = 7*modifier #time between words
 
 led = digitalio.DigitalInOut(board.GP16)
 led.direction = digitalio.Direction.OUTPUT
@@ -43,24 +43,24 @@ while True:
         translate += MORSE_CODE[message[letter]] + " " #translate letter and move to the next one 
     print (f"My translation: {translate}") #print translation
     
-    for letter in translate: 
-        if letter == ".":
-            led.value = True
-            time.sleep(dot_time)
-            led.value = False
-            time.sleep(dot_time)
+    for letter in translate: #for every character in the translated message
+        if letter == ".": #if the character is a dot
+            led.value = True #turn LED on 
+            time.sleep(dot_time) #wait for 0.25secs
+            led.value = False #turn LED off
+            time.sleep(between_taps) #wait 0.25 secs
         if letter == "-":
             led.value = True
             time.sleep(dash_time)
             led.value = False
-            time.sleep(dot_time)
+            time.sleep(between_taps)
         if letter == " ":
             led.value = True
             time.sleep(between_letters)
             led.value = False
-            time.sleep(dot_time)
+            time.sleep(between_taps)
         if letter == "/":
             led.value = True
             time.sleep(between_words)
             led.value = False    
-            time.sleep(dot_time)
+            time.sleep(between_taps)
